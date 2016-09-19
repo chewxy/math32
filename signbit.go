cf4072938f3927df44f7b68330bc7b707f23e7da
chewxy <chewxy@gmail.com>
2016-09-19 20:30:06 +1000

    Cleaned up licensing stuff. Didn't realize they'd look so fugly on godoc. Cleaned up readme too

--- signbit.go
+++ signbit.go
@@ -1,9 +1,3 @@
-// Copyright 2014 Xuanyi Chew. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-//
-// The original code is lifted from the Go standard library which is governed by
-// a BSD-style licence which can be found here: https://golang.org/LICENSE
 package math32
 
 // Signbit returns true if x is negative or negative zero.
