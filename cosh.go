186c369f3cce61c1e018f6b9a59268e1f1b36c14
Patricio Whittingslow <graded.sp@gmail.com>
2021-11-14 21:11:16 -0300

    hyperbolic: add sinh,cosh,tanh and inverse functions (#29)

--- cosh.go
+++ cosh.go
@@ -1,7 +1,10 @@
 package math32
 
-import "math"
-
 func Cosh(x float32) float32 {
-	return float32(math.Cosh(float64(x)))
+	x = Abs(x)
+	if x > 21 {
+		return Exp(x) * 0.5
+	}
+	ex := Exp(x)
+	return (ex + 1/ex) * 0.5
 }
